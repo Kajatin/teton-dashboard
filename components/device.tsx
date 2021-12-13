e9bd6b7937186f071fc556d9d70471759cb034d8
Roland Kajatin <roland.kajatin@gmail.com>
2021-12-13 22:04:33 +0100

    Added bed number and icons WIP

--- components/device.tsx
+++ components/device.tsx
@@ -3,27 +3,32 @@ import Indicator from './indicator'
 import styles from '../styles/Device.module.css'
 import IDevice from '../types/IDevice'
 import { getDeviceEnvironmentVariables } from '../lib/api'
+import React, { useState } from 'react';
 
 export default function Device(props: { device: IDevice }) {
-    const openLiveFeedForDevice = () => {
-        getDeviceEnvironmentVariables(props.device?.id).then(envVars => {
-            var roomNo: number = -1
-            var bedNo: number = -1
-            envVars.map((envVar) => {
-                const { name, value } = envVar
-                if (name == 'TETON_ROOM_NO')
-                    roomNo = value
-                if (name == 'TETON_BED_NO')
-                    bedNo = value
-            })
+    const [roomBed, setRoomBed] = useState("0.0");
 
-            const url = `https://data-tinder.vercel.app/live-feed?bedName=${roomNo}.${bedNo}`
-            window.open(url, "_blank")
+    getDeviceEnvironmentVariables(props.device?.id).then(envVars => {
+        var roomNo: number = -1
+        var bedNo: number = -1
+        envVars.map((envVar) => {
+            const { name, value } = envVar
+            if (name == 'TETON_ROOM_NO')
+                roomNo = value
+            if (name == 'TETON_BED_NO')
+                bedNo = value
         })
+
+        setRoomBed(`${roomNo}.${bedNo}`)
+    })
+
+    const openLiveFeedForDevice = () => {
+        const url = `https://data-tinder.vercel.app/live-feed?bedName=${roomBed}`
+        window.open(url, "_blank")
     }
 
     return (
-        <Link href={`/device/${props.device?.id}`}>
+        // <Link href={`/device/${props.device?.id}`}>
             <div className={styles.card}>
                 <div className={styles.topbar}>
                     <div className={styles.connection}>
@@ -32,18 +37,26 @@ export default function Device(props: { device: IDevice }) {
                             {props.device?.is_online ? "Online" : "Offline"}
                         </div>
                     </div>
-                    <div className={styles.name}>{props.device?.device_name}</div>
+                    <div className={styles.second_row}>
+                        <div className={styles.name}>{props.device?.device_name}</div>
+                        <div className={styles.bed}>
+                            <div className={styles.pillow} />
+                            <div className={styles.matress}>
+                                {roomBed}
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className={styles.content}>
                     <div className={styles.controls}>
                         <div className={styles.release}>
-                            {props.device?.id}
+                            v0.12.1+rev1
                         </div>
                         <div className={styles.buttons}>
-                            <button className={styles.button} onClick={openLiveFeedForDevice}></button>
-                            <div>
-                                {props.device?.id}
-                            </div>
+                            <button className={styles.button} onClick={openLiveFeedForDevice}>🎥</button>
+                            <Link href={`/device/${props.device?.id}`}>
+                                <button className={styles.button}>⌥</button>
+                            </Link>
                         </div>
                     </div>
                     <div className={styles.indicators}>
@@ -53,6 +66,6 @@ export default function Device(props: { device: IDevice }) {
                     </div>
                 </div>
             </div>
-        </Link>
+        // </Link>
     )
 }

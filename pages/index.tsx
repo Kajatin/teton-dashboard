55cd6d5e36fa61a7a06d5a0613e56cad12f6c4c1
Roland Kajatin <roland.kajatin@gmail.com>
2021-12-22 13:18:53 +0100

    Added error page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,45 @@ import Device from '../components/device'
 export default function Home() {
     const { data, error, mutate, isValidating } = useSWR('/api/devices', url => fetch(url).then(r => r.json()), { refreshInterval: 10000 })
 
-    if (error) return <div>failed to load</div>
-    if (!data) return <div>loading...</div>
+    const refreshPage = () => {
+        window.location.reload();
+    }
+
+    if (error) return (
+        <>
+            <Head>
+                <title>Teton Dashboard - Error</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link href='https://fonts.googleapis.com/css?family=Noto Sans' rel='stylesheet' />
+            </Head>
+
+            <main>
+                <div className={styles.error_overlay}>
+                    <div className={styles.error}>
+                        <div className={styles.redbar} />
+                        <div className={styles.error_body}>
+                            <div className={styles.oops}>
+                                Oops!
+                            </div>
+                            <div className={styles.wronghere}>
+                                Something's wrong here...
+                            </div>
+                            <div className={styles.explanation}>
+                                Could not fetch information about the devices from the Balena servers.
+                            </div>
+                            <div className={styles.contact}>
+                                Should the issue persist, contact us at <a href="mailto:roland@teton.ai">roland@teton.ai</a>.
+                            </div>
+                            <button className={styles.button} onClick={refreshPage}>
+                                <b>RELOAD</b>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+
 
     return (
         <>
